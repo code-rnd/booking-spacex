180f2eaa3635cc434dfc8a803216310f685ff329
Шаталов Роман <shatalovrs@it2g.ru>
2022-11-11 01:20:56 +0300

    - Ант дезайн - логика и стилизация карточки

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,46 @@
 import { FC } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { Card as AntCard, PageHeader, Image } from "antd";
 import { useCurrentCard } from "./Card.hooks";
+import s from "./Card.module.scss";
+
+const { Meta } = AntCard;
 
 export const Card: FC = () => {
   const [card, pending] = useCurrentCard();
   const navigate = useNavigate();
 
+  const { name, links } = card || {};
+
   return (
-    <div>
-      <div>{pending && !!card ? "loader" : <div>{card?.name}</div>}</div>
-      <div onClick={() => navigate(-1)}>go back</div>
+    <div className={s.container}>
+      <PageHeader onBack={() => navigate(-1)} title={name} subTitle="/ Main" />
+      <AntCard
+        hoverable
+        style={{ width: "250px" }}
+        cover={
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "10px",
+            }}
+          >
+            <Image preview={false} width={250} src={links?.patch?.large} />
+          </div>
+        }
+        loading={pending}
+      >
+        <Meta
+          title="Youtube"
+          description={"https://www.youtube.com/watch?v=" + links?.youtube_id}
+        />
+        <Meta title="Wikipedia" description={links?.wikipedia} />
+        <Meta title="Presskit" description={links?.presskit} />
+        <Meta title="Webcast" description={links?.webcast} />
+        <Meta title="Article" description={links?.article} />
+      </AntCard>
     </div>
   );
 };
